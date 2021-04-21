1eea6eef2c9eac26a1bd492d88f69e0e4a63f7ed
hamz4arif <hamza.arif486@gmail.com>
2021-04-21 18:21:58 +0500

    video 300

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,98 @@
+import { HttpClient, HttpErrorResponse } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Subject, throwError } from "rxjs";
+import { catchError, tap } from "rxjs/operators";
+import { User } from "./user.model";
+
+export interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+  registered?: string;
+}
+@Injectable({
+  providedIn: "root",
+})
+export class AuthService {
+  user = new Subject<User>();
+
+  constructor(private http: HttpClient) {}
+  signup(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBkfxs_kMztVfSSP6Jh-nCYlmRD-F5JA20",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(
+        catchError(this.HandleError),
+        tap((resData) => {
+          this.HandleAuthUser(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        })
+      );
+  }
+
+  signIn(email: string, password: string) {
+    return this.http
+      .post<AuthResponseData>(
+        "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBkfxs_kMztVfSSP6Jh-nCYlmRD-F5JA20",
+        {
+          email: email,
+          password: password,
+          returnSecureToken: true,
+        }
+      )
+      .pipe(catchError(this.HandleError), tap((resData) => {
+          this.HandleAuthUser(
+            resData.email,
+            resData.localId,
+            resData.idToken,
+            +resData.expiresIn
+          );
+        }));
+  }
+  private HandleError(errorResponse: HttpErrorResponse) {
+    let ErrorMessage = "An Error Occured!";
+    if (!errorResponse.error || !errorResponse.error.error) {
+      return throwError(ErrorMessage);
+    }
+    switch (errorResponse.error.error.message) {
+      case "EMAIL_EXISTS":
+        ErrorMessage =
+          " The email address is already in use by another account.";
+        break;
+      case "EMAIL_NOT_FOUND":
+        ErrorMessage =
+          " There is no user record corresponding to this identifier. The user may have been deleted.";
+        break;
+      case "INVALID_PASSWORD":
+        ErrorMessage =
+          " The password is invalid or the user does not have a password.";
+        break;
+      // console.log(ErrorMessage)
+    }
+    return throwError(ErrorMessage);
+  }
+
+  private HandleAuthUser(
+    email: string,
+    userID: string,
+    token: string,
+    expiresIn: number
+  ) {
+    const tokenExpiry = new Date(new Date().getTime() + expiresIn * 1000);
+    const user = new User(email, userID, token, tokenExpiry);
+    console.log(user)
+    this.user.next(user);
+  }
+}
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,19 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { Subscription } from "rxjs";
+import { AuthService } from "../auth/auth.service";
 
-import { DataStorageService } from '../shared/data-storage.service';
+import { DataStorageService } from "../shared/data-storage.service";
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html'
+  selector: "app-header",
+  templateUrl: "./header.component.html",
 })
-export class HeaderComponent {
-  constructor(private dataStorageService: DataStorageService) {}
-
+export class HeaderComponent implements OnInit, OnDestroy {
+  authObs: Subscription;
+  isAuthentificated = false;
+  constructor(
+    private dataStorageService: DataStorageService,
+    private authSer: AuthService
+  ) {}
+  ngOnInit() {
+    this.authObs = this.authSer.user.subscribe((user) => {
+      this.isAuthentificated = !!user;
+    });
+  }
   onSaveData() {
     this.dataStorageService.storeRecipes();
   }
 
   onFetchData() {
     this.dataStorageService.fetchRecipes().subscribe();
+  }
+  ngOnDestroy() {
+    this.authObs.unsubscribe();
   }
 }
