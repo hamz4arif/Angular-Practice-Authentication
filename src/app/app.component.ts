9b9dd6b72b0eb9126af1c26e830216a043eda218
hamz4arif <hamza.arif486@gmail.com>
2021-04-22 16:07:08 +0500

    animation Completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,128 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from './auth/auth.service';
+import {
+  animate,
+  group,
+  keyframes,
+  state,
+  style,
+  transition,
+  trigger,
+} from "@angular/animations";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  animations: [
+    trigger("divState", [
+      state(
+        "normal",
+        style({
+          "background-color": "red",
+          transform: "translateX(0)",
+        })
+      ),
+      state(
+        "highlighted",
+        style({
+          "background-color": "blue",
+          transform: "translateX(100px)",
+        })
+      ),
+      transition("normal<=>highlighted", animate(500)),
+    ]),
+    trigger("wildState", [
+      state(
+        "normal",
+        style({
+          "background-color": "red",
+          transform: "translateX(0) scale(1)",
+        })
+      ),
+      state(
+        "highlighted",
+        style({
+          "background-color": "blue",
+          transform: "translateX(100px) scale(1)",
+        })
+      ),
+      state(
+        "shrunkin",
+        style({
+          "background-color": "green",
+          transform: "translateX(0px) scale(0.5)",
+        })
+      ),
+      transition("normal<=>highlighted", animate(500)),
+      transition("shrunkin<=>*", [
+        style({
+          "background-color": "orange",
+        }),
+        style({ borderRadius: "50px" }),
+        animate(500),
+      ]),
+    ]),
+    trigger("list1", [
+      transition("void=>*", [
+        style({
+          opacity: "1",
+          transform: "translateX(-100px)",
+        }),
+        group([
+          animate(
+            100,
+            style({
+              color: "red",
+              fontSize:'50px'
+            })
+          ),
+          animate(
+            500,
+            style({
+              opacity: 1,
+              transform: "translateX(-100px)",
+            })
+          ),
+          animate(300),
+        ]),
+      ]),
+      transition("*=>void", [
+        style({
+          transform: "translateX(100px)",
+          opacity: "10",
+        }),
+        animate(500),
+      ]),
+    ]),
+  ],
 })
-export class AppComponent implements OnInit {
-  constructor(private authSer:AuthService){}
-  ngOnInit(){
-    this.authSer.autoLogin();
+export class AppComponent {
+  state = "normal";
+  wildstate = "normal";
+  list = ["Milk", "Sugar", "Bread"];
+
+  onAdd(item) {
+    this.list.push(item);
   }
-  
+  onDelete(val) {
+    console.log(val);
+    this.list.splice(val, 1);
+  }
+  onChange() {
+    this.state == "normal"
+      ? (this.state = "highlighted")
+      : (this.state = "normal");
+
+    this.wildstate == "normal"
+      ? (this.wildstate = "highlighted")
+      : (this.wildstate = "normal");
+  }
+  onShrunk() {
+    this.wildstate = "shrunkin";
+  }
+  onanimationStart(event){
+    console.log(event)
+  }
+  onanimationDone(event){
+    console.log(event)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,58 +1,21 @@
-import { BrowserModule } from "@angular/platform-browser";
-import { NgModule } from "@angular/core";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from "./app.component";
-import { HeaderComponent } from "./header/header.component";
-import { RecipesComponent } from "./recipes/recipes.component";
-import { RecipeListComponent } from "./recipes/recipe-list/recipe-list.component";
-import { RecipeDetailComponent } from "./recipes/recipe-detail/recipe-detail.component";
-import { RecipeItemComponent } from "./recipes/recipe-list/recipe-item/recipe-item.component";
-import { ShoppingListComponent } from "./shopping-list/shopping-list.component";
-import { ShoppingEditComponent } from "./shopping-list/shopping-edit/shopping-edit.component";
-import { DropdownDirective } from "./shared/dropdown.directive";
-import { ShoppingListService } from "./shopping-list/shopping-list.service";
-import { AppRoutingModule } from "./app-routing.module";
-import { RecipeStartComponent } from "./recipes/recipe-start/recipe-start.component";
-import { RecipeEditComponent } from "./recipes/recipe-edit/recipe-edit.component";
-import { RecipeService } from "./recipes/recipe.service";
-import { AuthComponent } from "./auth/auth.component";
-import { LoadingComponent } from "./shared/loading/loading.component";
-import { Authinterceptorservice } from "./auth/Auth.interceptor.service";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
-    ShoppingListComponent,
-    ShoppingEditComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
-    AuthComponent,
-    LoadingComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
-    HttpClientModule,
-    AppRoutingModule,
+    BrowserAnimationsModule
   ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: Authinterceptorservice,
-      multi: true,
-    },
-  ],
-  bootstrap: [AppComponent],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule { }
